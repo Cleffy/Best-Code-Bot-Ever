4ea0e0c9a10627159871a971faca6d66d756170c
Cleffy <falchard@live.com>
2023-10-07 19:18:41 -0700

    Update server.js
    Adjusted server for __dirname in ES6, might have an error on Mac

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
-import path from'path';
+import path from 'path';
+import { URL } from 'url';
 
-import { authMiddleware } from'./utils/auth.js';
-
-import { typeDefs, resolvers } from'./schemas/index.js';
+import { authMiddleware } from './utils/auth.js';
+import { typeDefs, resolvers } from './schemas/index.js';
 import db from './config/connection.js';
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname).replace(/^\\([A=Z]:\\)/, '$1');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -16,27 +17,28 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-  }
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
   
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/'));
-  })
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/'));
+})
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
   
-  const startApolloServer = async (typeDefs, resolvers) => {
-    await server.start();
-    server.applyMiddleware({ app });
-    
-    db.once('open', () => {
-      app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}!`);
-        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-      })
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
-    };
-    
-    startApolloServer(typeDefs, resolvers);
+  })
+  };
+  
+  startApolloServer(typeDefs, resolvers);
