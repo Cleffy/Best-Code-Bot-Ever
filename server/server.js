f25ab6207a8be2b93491ae9bafcd4737fa342549
Cleffy <falchard@live.com>
2023-10-10 22:50:36 -0700

    Undid changes to static directory

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('/client/build'));
+  app.use(express.static('../client/build'));
 }
   
 app.get('/', (req, res) => {
