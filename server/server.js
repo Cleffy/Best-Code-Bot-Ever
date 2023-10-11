b1d80106c1662e5706423d45e839d9331d1973d5
Cleffy <falchard@live.com>
2023-10-10 22:47:19 -0700

    Check client build

--- server/server.js
+++ server/server.js
@@ -24,11 +24,11 @@ app.use(express.json());
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static('/client/build'));
 }
   
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/'));
+  res.sendFile(path.join(__dirname, '../client'));
 })
 
 const startApolloServer = async (typeDefs, resolvers) => {
