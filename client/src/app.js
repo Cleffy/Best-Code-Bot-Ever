90e1c83f32e2b2b895c6f545ac28ff241e650bd8
chandler ames <eddiehawk1991@gmail.com>
2023-10-10 17:19:53 -0700

    app.js

---
+++ client/src/app.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Menu from './Menu'; 
+
+const App = () => {
+  
+};
+
+export default App;
