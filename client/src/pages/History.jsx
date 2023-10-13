1e5ffbcc99d68c5ae62aa686983cc4b7ba807583
GimmeKitties711 <131449161+GimmeKitties711@users.noreply.github.com>
2023-10-12 17:58:19 -0700

    i hate it here i hate it here i hate it here

--- client/src/pages/History.jsx
+++ client/src/pages/History.jsx
@@ -11,12 +11,12 @@ import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
 import { QUERY_CHAT } from '../utils/queries';
 
-const { loading, data } = useQuery(QUERY_CHAT);
-const userChats = data?.userChats || {}; // no idea if this is going to work
-console.log('data: ', data);
-console.log('userChats: ', userChats);
-
 const History = () => {
+
+    const { loading, data } = useQuery(QUERY_CHAT);
+    const userChats = data?.userChats || {}; // no idea if this is going to work
+    console.log('data: ', data);
+    console.log('userChats: ', userChats);
 
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -28,49 +28,49 @@ const History = () => {
         return <h2>Loading...</h2>;
     }
 
-    return (
-        <Container>
-            <Row>
-                <Col>
-                    <Card>
-                        <Card.Body>
-                            <Card.Title>History</Card.Title>
-                            <Card.Text>
-                                <Button>
-                                    Chat 1
-                                </Button>
-                            </Card.Text>
-                            <Card.Text>
-                                <Button>
-                                    Chat 2
-                                </Button>
-                            </Card.Text>
-                            <Card.Text>
-                                <Button>
-                                    Chat 3
-                                </Button>
-                            </Card.Text>
-                            <Card.Text>
-                                <Button>
-                                    Chat 4
-                                </Button>
-                            </Card.Text>
-                            <Card.Text>
-                                <Button>
-                                    Chat 5
-                                </Button>
-                            </Card.Text>
-                            <Card.Text>
-                                <Button>
-                                    Chat 6
-                                </Button>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
-    );
+    // return (
+    //     <Container>
+    //         <Row>
+    //             <Col>
+    //                 <Card>
+    //                     <Card.Body>
+    //                         <Card.Title>History</Card.Title>
+    //                         <Card.Text>
+    //                             <Button>
+    //                                 Chat 1
+    //                             </Button>
+    //                         </Card.Text>
+    //                         <Card.Text>
+    //                             <Button>
+    //                                 Chat 2
+    //                             </Button>
+    //                         </Card.Text>
+    //                         <Card.Text>
+    //                             <Button>
+    //                                 Chat 3
+    //                             </Button>
+    //                         </Card.Text>
+    //                         <Card.Text>
+    //                             <Button>
+    //                                 Chat 4
+    //                             </Button>
+    //                         </Card.Text>
+    //                         <Card.Text>
+    //                             <Button>
+    //                                 Chat 5
+    //                             </Button>
+    //                         </Card.Text>
+    //                         <Card.Text>
+    //                             <Button>
+    //                                 Chat 6
+    //                             </Button>
+    //                         </Card.Text>
+    //                     </Card.Body>
+    //                 </Card>
+    //             </Col>
+    //         </Row>
+    //     </Container>
+    // );
 }
 
 export default History;
