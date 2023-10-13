0c984cbd9a0ed2e2f2acc5a273ae7ed634e70ff1
Cleffy <falchard@live.com>
2023-10-12 20:19:51 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.get('/', (req, res) => {
 */
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 
