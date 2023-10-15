26f940b1770473119945938f3f878ed30d4aa3f1
MMerzoug <monique.merzoug@icloud.com>
2023-10-15 03:19:37 -0400

    fixed chat usernames and added loading spinner

---
+++ client/src/components/Spinner.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Spinner(){
+    return (
+        <div className="spinner-container">
+            <div className="load-spinner"></div>
+            <p>let me think about that</p>
+        </div>
+    )
+}
